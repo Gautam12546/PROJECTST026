4c29b3e37d1310efc047b2a65473eaaf563cccb0
Gautam12546 <gautamsi0000@gmail.com>
2024-04-18 12:54:17 +0530

    updated profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 // import Loginsign from './Components/Loginsign';
 // import Belowbx from './Components/Belowbx'
@@ -6,34 +6,22 @@
 
 import Profile from './Components/Profile';
 // import UserProfileCard from './Components/UserProfileCard'
-=======
 import React from 'react';
 import Loginsign from './Components/Loginsign';
 import Belowbx from './Components/Belowbx'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Cart from './Components/Cart';
 import Button from './Components/Button';
->>>>>>> 2793dede1c8576ab110cb191668b8a6fed954006
 function App() {
   return (
-<<<<<<< HEAD
-     <div>
-       {/* <Card/>
-      <Loginsign/> */}
-      {/* <Belowbx/> */}
-      {/* <UserProfileCard/> */}
-      <Profile/>
-
-     </div>
-=======
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Belowbx />} />
         <Route path="/cart" element={<Cart button={<Button />}/>} />
         <Route path="/signup" element={<Loginsign />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-    </BrowserRouter>
->>>>>>> 2793dede1c8576ab110cb191668b8a6fed954006
+      </BrowserRouter>
   )
 }
 
--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -6,68 +6,62 @@ import { MdHistory } from "react-icons/md";
 import FoodCategory from "./FoodCategory";
 import { BsCartCheckFill } from "react-icons/bs";
 import { MdEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 const Profile = () => {
   return (
-    <div className=" flex items-center justify-center h-screen w-screen bg-slate-700">
-      <div className="overflow-hidden h-4/5 w-11/12 rounded-[30px] flex flex-col bg-zinc-600">
-        <nav className="h-1/5 font-medium text-yellow-400 flex flex-row text-4xl   items-start justify-between bg-zinc-600 p-6">
-          <a className=" cursor-pointer" href="">
+    <div className="flex items-center justify-center min-h-screen bg-slate-700">
+      <div className="overflow-hidden w-full sm:w-11/12 lg:w-3/4 xl:w-2/3 rounded-lg flex flex-col bg-zinc-600">
+        <nav className="h-1/5 font-medium text-yellow-400 flex flex-row text-4xl items-start justify-between bg-zinc-600 p-6">
+          <Link to='/'>
             <IoIosArrowBack />
-          </a>
-          <a className="" href="">
-            Profile
-          </a>
-          <a className="cursor-pointer" href="">
+          </Link>
+          <span className="text-xl sm:text-2xl">Profile</span>
+          <Link to='/cart'>
             <FaCartPlus />
-          </a>
+          </Link>
         </nav>
 
-        <fieldset className="w-full h-screen flex flex-col bg-yellow-500 overflow-scroll">
+        <fieldset className="flex flex-col bg-yellow-500 overflow-scroll">
           <legend className="text-center rounded-full h-40 w-40 overflow-hidden">
             <img
+              className="object-cover w-full h-full"
               src="https://images.pexels.com/photos/3714432/pexels-photo-3714432.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
               alt="Userprofile Image"
             />
           </legend>
 
-          <div className="w-full gap-8 flex flex-col  items-center bg-yellow-500">
-            <div className=" w-full flex flex-col items-center gap-1 ">
-
-              <div className=" items-center justify-center  flex gap-2 flex-row">
-                <h2 className="text-3xl font-semibold">Username</h2> <p className="text-2xl">
+          <div className="p-6 sm:p-8">
+            <div className="flex sm:flex-row items-center justify-center gap-4">
+              <h2 className="text-3xl font-semibold">Username</h2>
+              <p className="text-2xl cursor-pointer">
                 <MdEdit />
-              </p> </div>
-              <p className="text-sm font-semibold text-slate-300">
-                description about userfkjfgbjf
               </p>
-
-            </div>
-
-            <div className="w-full  flex flex-row justify-evenly items-center">
-              <a className="text-6xl text-black-400" href="">
-                <MdHome />
-              </a>
-              <a className="text-5xl text-black-400" href="">
-                <BsCartCheckFill />
-              </a>
-              <a className="text-5xl text-black-400" href="">
-                <MdHistory />
-              </a>
-            </div>
-          </div>
-
-          <div className=" flex flex-col w-full items-center justify-center gap-4 p-8">
-            <p className="font-extrabold text-xl">New</p>
-            <div className=' flex flex-row items-center gap-3 h-3/6 overflow-hidden'>
-         <FoodCategory/> 
-         <FoodCategory/>
-         <FoodCategory/>
-         <FoodCategory/>
-         
             </div>
             
+          </div>
 
-            <p className="font-extrabold text-xl">Top Sales</p>
+          <div className="flex justify-evenly items-center p-6 sm:p-8">
+            <a className="text-6xl text-black-400" href="">
+              <MdHome />
+            </a>
+            <a className="text-5xl text-black-400" href="">
+              <BsCartCheckFill />
+            </a>
+            <a className="text-5xl text-black-400" href="">
+              <MdHistory />
+            </a>
+          </div>
+
+          <div className="p-6 sm:p-8 flex flex-col items-center">
+            <p className="font-extrabold text-xl">New</p>
+            <div className="flex flex-row items-center gap-3 overflow-x-auto">
+              <FoodCategory />
+              <FoodCategory />
+              <FoodCategory />
+              <FoodCategory />
+            </div>
+            <p className="font-extrabold text-xl mt-4">Top Sales</p>
           </div>
         </fieldset>
       </div>
